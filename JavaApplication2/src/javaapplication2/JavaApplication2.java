9a031c536a376a5672748eed05e128f5593d0e39
Labing I5 <Labing I5@LabingI5-25-101>
2017-10-04 17:27:34 -0500

    adelante lo boorado :)

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,21 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication2;
 
-/**
- *
- * @author Labin
- */
+import java.util.Scanner;
+
+
 public class JavaApplication2 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a;
+        int b;
+        int n;
+        int arm;
+        int v;
+        char [][] mapa;
+        
+        //longitud del mapa
+        Scanner lector= new Scanner (System.in);
+        System.out.println("Hola señor usuario");
+        System.out.println("Porfavor digite el tamaño de la longitud A");
+        a= lector.nextInt();
+        System.out.println("Porfavor digite el tamaño de la longitud B");
+        b= lector.nextInt();
+        mapa= new char [a][b];
+        
+        //numero de jugadores
+        System.out.println("Porfavor digite la cantidad de jugadores");
+        n=lector.nextInt();
+        if (n<10){
+            System.out.println("Cantidad de jugadores permitida");
+        }
+        else {
+            System.out.println("Cantidad de jugadores excedida");
+        }
+        //cantidad de armas
+       System.out.println("Cantidad de jugadores permitida"); 
+       arm=lector.nextInt();
+       
+       //cantidad de curas
+       System.out.println("Inserte el numero de vidas");
+       v=lector.nextInt();
+       
+       //mapa
+       for (int i=0;i<a;i++)
+           for (int j=0;j<b;j++)
+              if (i==0||j==0||i==b-1||j==a-1)
+              
+       mapa [i][j]= 'X';
+       
+              else{
+                  mapa [i][j]=' ';
+              }
+       for (int i=0;i<a;i++){
+           
+           for (int j=0;j<b;j++){
+             System.out.print(mapa[i][j]);
+             System.out.println("");
+           }
+           
+       }
     }
     
 }
